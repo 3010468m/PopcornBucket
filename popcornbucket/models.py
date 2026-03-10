5e0639a879955f1903ac104bb6b19d16c7faf56b
Abigail Martin <3010468m@student.gla.ac.uk>
2026-03-10 19:02:45 +0000

    django project template

--- popcornbucket/models.py
+++ popcornbucket/models.py
@@ -4,15 +4,3 @@ import datetime
 
 # Create your models here.
     
-class Review(models.Model):
-    TEXT_MAX_LENGTH = 4000; 
-
-    review_id = models.IntegerField(unique=True)
-    rating = models.IntegerField(default=1)
-    review_text = models.CharField(max_length= TEXT_MAX_LENGTH)
-    created_at = models.DateTimeField(default=datetime.datetime.now())
-    user_id = models.ForeignKey(User.id, on_delete=models.CASCADE)
-    movie_id = models.ForeignKey(Movie.id, on_delete=models.CASCADE)
-
-
-class Movie(models.Model):
