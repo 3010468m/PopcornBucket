7ff479670eaab0ecc12d7ba6d3b570f60fb5897c
Simi Adedokun <3007649a@student.gla.ac.uk>
2026-03-11 16:47:20 +0000

    Server up and running

--- popcornbucket_project/settings.py
+++ popcornbucket_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movies'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'popcornbucket.urls'
+ROOT_URLCONF = 'popcornbucket_project.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'popcornbucket.wsgi.application'
+WSGI_APPLICATION = 'popcornbucket_project.wsgi.application'
 
 
 # Database
