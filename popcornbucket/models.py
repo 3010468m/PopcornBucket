7174caabbe5abe08e4bf8a8b255a18f44a553126
Abigail Martin <3010468m@student.gla.ac.uk>
2026-03-16 19:07:07 +0000

    average iflm rating function added to film model, and shown on film list

--- popcornbucket/models.py
+++ popcornbucket/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.utils.timezone import now
 import datetime
 from django.db import models
+from django.db.models import Avg
 # Create your models here.
 
 
@@ -20,6 +21,9 @@ class Film(models.Model):
     description = models.TextField()
     poster = models.ImageField(upload_to='film_posters/', blank=True, null=True)
     release_year = models.IntegerField(null=True, blank=True)
+    
+    def avg_rating(self):
+        return self.review_set.aggregate(Avg('rating'))['rating__avg']
 
     def __str__(self):
         return self.title
@@ -28,7 +32,7 @@ class Film(models.Model):
 class Review(models.Model):
     TEXT_MAX_LENGTH = 4000; 
 
-    rating = models.IntegerField(default=1)
+    rating = models.IntegerField(default=0)
     review_text = models.CharField(max_length= TEXT_MAX_LENGTH)
     created_at = models.DateTimeField(default=now)
     up_votes = models.IntegerField(default=0)
@@ -36,5 +40,4 @@ class Review(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     film = models.ForeignKey(Film, on_delete=models.CASCADE)
-
 
