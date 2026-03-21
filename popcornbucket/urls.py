a9dbc29a5d63527467343a4bfc84c569fcefcff6
Simi Adedokun <3007649a@student.gla.ac.uk>
2026-03-21 17:32:49 +0000

    Implement watchlist feature with add/remove functionality and update templates

--- popcornbucket/urls.py
+++ popcornbucket/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.film_list, name='film_list'),
     path('<int:id>/', views.film_detail, name='film_detail'),
-    
+    path('watchlist/add/<int:film_id>/', views.add_to_watchlist, name='add_to_watchlist'),
+    path('watchlist/remove/<int:film_id>/', views.remove_from_watchlist, name='remove_from_watchlist'),
 ]
